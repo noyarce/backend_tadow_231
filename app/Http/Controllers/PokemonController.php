4cc59100f8599449c92ef9d83c346716b8cdba7d
Nicolas Oyarce V <nicolas.oyarce@mundotelecomunicaciones.cl>
2023-05-10 17:00:50 -0400

    wip 10 may

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pokemon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class PokemonController extends Controller
+{
+    public function createPokemon(Request $request){
+        Log::info(["REquest"=>$request->all()]);
+
+        $pokemon = new Pokemon();
+        $pokemon->nombre=$request->nombre;
+        $pokemon->foto=$request->foto;
+        $pokemon->save();
+
+        return $pokemon;
+
+    }
+
+    public function listarPokemones(Request $request){
+        $pokemon = Pokemon::all();
+        return $pokemon;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PokemonController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/tst')->group(function () use ($router) {
+    $router->post('posteo', [PokemonController::class, 'createPokemon']);
+    $router->get('all',[PokemonController::class, 'listarPokemones']);
+   
 });
