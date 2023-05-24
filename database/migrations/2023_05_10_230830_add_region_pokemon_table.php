870cce51babd44876c2f802264c3e78201f69857
Nicolas Oyarce V <nicolas.oyarce@mundotelecomunicaciones.cl>
2023-05-24 18:09:13 -0400

    fix

--- database/migrations/2023_05_10_230830_add_region_pokemon_table.php
+++ database/migrations/2023_05_10_230830_add_region_pokemon_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
      */
     public function down()
     {
-         Schema::table('pokemon', function (Blueprint $table) {
+        Schema::table('pokemon', function (Blueprint $table) {
             $table->dropForeign('region_id');
         });
     }
