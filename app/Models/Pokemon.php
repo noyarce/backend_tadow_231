cb91ef4acfff941e3764d6dd59fc1b1e5e8b935c
Nicolas Oyarce V <nicolas.oyarce@mundotelecomunicaciones.cl>
2023-05-24 09:58:03 -0400

    wip

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -9,4 +9,18 @@ class Pokemon extends Model
 {
     use HasFactory;
     public $table = 'pokemon';
+    protected $primaryKey = 'id';
+    public $incrementing = true;
+
+
+public function region()
+    {
+        return $this->belongsTo(Region::class, 'region_id');
+    } 
+
+    public function tipo()
+    {
+        return $this->belongsTo(Region::class, 'tipo_id');
+    } 
+
 }
--- app/Models/Region.php
+++ app/Models/Region.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Region extends Model
 {
     use HasFactory;
+
+    public function pokemon()
+    {
+        return $this->hasMany(Pokemon::class);
+    }
+
+
+
 }
